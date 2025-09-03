b8dc9a96ac7f4ad90157b40b21f9794c45c906c6
Vishu <vishujain.activa@gmail.com>
2025-09-03 12:46:44 +0530

    Update vite.config.js

--- TodoList/vite.config.js
+++ TodoList/vite.config.js
@@ -4,18 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  server: {
-    proxy: {
-      "/task": {
-        target: "https://todolist-1-snqc.onrender.com",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/user": {
-        target: "https://todolist-1-snqc.onrender.com",
-        changeOrigin: true,
-        secure: false,
-      },
-    }
-  }
 })
