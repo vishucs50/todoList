a5f2ab9e0875ec182dd74ac5e9404fb929d4da9c
Vishu <vishujain.activa@gmail.com>
2025-09-03 12:35:36 +0530

    Update api.js

--- TodoList/src/api.js
+++ TodoList/src/api.js
@@ -1,1 +1,10 @@
 
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.PROD
+    ? "https://your-backend.onrender.com"
+    : "http://localhost:3000"
+});
+
+export default api;
