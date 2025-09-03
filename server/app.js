c808a3efc97abea39c00880f5a761e0de6c552a9
Vishu <vishujain.activa@gmail.com>
2025-09-03 11:38:16 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,35 +1,22 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-
-const todoRoute = require("./router/todo");
-const userRoute = require("./router/user");
+require('dotenv').config()
+const express=require('express');
+const app=express();
+const cors=require('cors');
+const todoRoute=require('./router/todo')
 const Task = require("./models/task");
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-const dbUrl = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/TodoList";
-
+const userRoute=require('./router/user')
 app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
+const mongoose = require("mongoose");
+const PORT=3000;
+const dbUrl =process.env.MONGO_URI || "mongodb://127.0.0.1:27017/TodoList";
+mongoose.connect(dbUrl);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => console.log("Database connected"));
+app.use('/task', todoRoute);
+app.use('/user',userRoute);
 
-// MongoDB connection
-mongoose
-  .connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Database connected"))
-  .catch((err) => console.error("DB connection error:", err));
-
-
-// Routes
-app.use("/task", todoRoute);
-app.use("/user", userRoute);
-
-// Error handler
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: "Something went wrong!" });
-});
-
-// Start server
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT,()=>{
+    console.log("Listening on port 3000");
+})
